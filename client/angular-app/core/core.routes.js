00be3a6eb2335719d5715d3a1641c7d6e7457829
Sean Large <sean.large1@gmail.com>
2016-10-02 18:42:44 -0400

    add base module's, login and authentication services

---
+++ client/angular-app/core/core.routes.js
@@ -0,0 +1,43 @@
+(function () {
+  'use strict';
+
+  // Handles all the logic for navigation within the app
+  // Declare app level module which depends on views, and components
+  angular.module('app')
+
+    .config(['$stateProvider', '$urlRouterProvider', '$resourceProvider', '$locationProvider', '$httpProvider',
+      function ($stateProvider, $urlRouterProvider, $resourceProvider, $locationProvider, $httpProvider) {
+
+        $stateProvider.decorator('parent', function (internalStateObj, parentFn) {
+          // This fn is called by StateBuilder each time a state is registered
+
+          // The first arg is the internal state. Capture it and add an accessor to public state object.
+          internalStateObj.self.$$state = function () {
+            return internalStateObj;
+          };
+
+          // pass through to default .parent() function
+          return parentFn(internalStateObj);
+        });
+
+        // Don't strip trailing slashes from calculated URLs
+        $resourceProvider.defaults.stripTrailingSlashes = false;
+
+        $urlRouterProvider.otherwise('/login');
+        $locationProvider.html5Mode(true);
+
+        $stateProvider
+          .state('login', {
+            url: '/login',
+            templateUrl: 'modules/login/login.html',
+            controller: 'LoginCtrl',
+            controllerAs: 'loginCtrl',
+            title: 'Login'
+          });
+
+       
+
+        $httpProvider.interceptors.push('ErrorInterceptor');
+      }
+    ]);
+})();
---
+++ client/angular-app/core/dependencies/core.modules.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  // Includes the essential angular modules defined in angular-app/modules directory
+  angular.module('hs.modules', [
+    // 'hs.products',
+    // 'hs.retailer',
+    // 'hs.manufacturer',
+    'hs.login'
+  ])
+})()
