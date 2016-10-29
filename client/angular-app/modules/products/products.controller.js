508bc1d8d6bbc4bca9c7c1c44831b80ab2dc2c28
Sean Large <sean.large1@gmail.com>
2016-10-29 14:39:25 -0400

    comment out http call to see it that's what crashing the app

--- client/angular-app/modules/products/products.controller.js
+++ client/angular-app/modules/products/products.controller.js
@@ -1,20 +1,20 @@
-(function(){
- 'use strict';
+(function() {
+    'use strict';
 
-    angular.module('products.controller',[ ])
+    angular.module('products.controller', [])
 
-   
-    .controller('productsController', ['$http', '$scope', 'endpoints', function($http, $scope, enpoints){
+
+    .controller('productsController', ['$http', '$scope', 'endpoints', function($http, $scope, enpoints) {
 
         console.log('Products Controller locked and loaded');
-        
-         $http.get(enpoints.products)
-                .success(function(data){
-                    console.log(data, '---------------PRODUCT DATA');
-                })
-                .error(function(err){
-                    console.log(err);
-                })
+
+        //  $http.get(enpoints.products)
+        //         .success(function(data){
+        //             console.log(data, '---------------PRODUCT DATA');
+        //         })
+        //         .error(function(err){
+        //             console.log(err);
+        //         })
 
 
 
