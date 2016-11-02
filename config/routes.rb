cae9302535aeed535927de5c19281d7ece910b3d
Sean Large <sean.large1@gmail.com>
2016-11-01 23:35:37 -0400

    change root back to products#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,7 @@ Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # match "/*path" => redirect("/?goto=%{path}")
 
-  class XHRConstraint
-  # change ::Rails.env != 'test' for different environemnts below i.e production, development etc.. 
-    def matches?(request)
-      !request.xhr? && !(request.url =~ /\.json$/ && ::Rails.env != 'test')
-    end
-  end
-
-   match "/*path" => redirect("/?goto=%{path}"), :via => [:get, :post, :put, :delete], :constraints => XHRConstraint.new
-
-
+  root 'products#index'
 
 end
