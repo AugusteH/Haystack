20d76608b61d347a6f0f361f8fdb0e08b92cdacd
Steven Fleming <sfleming@optoro.com>
2017-01-07 14:31:01 -0500

    BE-16 spec for user create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  describe '#create' do
+
+    let(:attributes) { {email: 'derp@derp.com', password: 'password'} }
+    it 'creates a new user' do
+      expect{post :create, params: {user: attributes}, format: :json}.to change(User, :count).by(1)
+    end
+
+  end
 end
